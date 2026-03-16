b692fa4c88c7252a1b1a68869add24af9f172c44
elimelech <ee525566@gmail.com>
2026-03-16 11:02:53 +0200

    create form in AddLauncher page

--- frontend/launchersApp/src/pages/AddLauncher.jsx
+++ frontend/launchersApp/src/pages/AddLauncher.jsx
@@ -2,27 +2,35 @@ import React from 'react'
 
 export default function AddLauncher() {
     return (
-        <form>
-            <div>
-                <label htmlFor="name">name:</label>
-                <input type="text" name='name'/>
-            </div>
-            <div>
-                <label htmlFor="rocketType">rocketType:</label>
-                <input type='checkbox' name='rocketType'></input>
-            </div>
-            <div>
-                <label htmlFor="latitude">latitude:</label>
-                <input type="number" name='latitude'/>
-            </div>
-            <div>
-                <label htmlFor="longitude">longitude:</label>
-                <input type="number" name='longitude'/>
-            </div>
-            <div>
-                <label htmlFor="city">city:</label>
-                <input type="text" name='city'/>
-            </div>
-        </form>
+        <div className='w-full h-screen justify-center'>
+            <form className='p-3 gap-5 border justify-around rounded flex flex-col w-[30%]'>
+                <div className='flex flex-col justify-between'>
+                    <label htmlFor="name">name:</label>
+                    <input placeholder='enter name' className='pl-1 w-[80%] border rounded border-black' type="text" name='name' />
+                </div>
+                <div className='flex flex-col justify-between'>
+                    <label htmlFor="rocketType">rocketType:</label>
+                    <select className='w-[40%] border rounded border-black' name="rocketType" id="rocketType">
+                        <option value="Choose">choose</option>
+                        <option value="Shahab3">Shahab3</option>
+                        <option value="Fetah110">Fetah110</option>
+                        <option value="Radwan">Radwan</option>
+                        <option value="Kheibar">Kheibar</option>
+                    </select>
+                </div>
+                <div className='flex flex-col justify-between'>
+                    <label htmlFor="latitude">latitude:</label>
+                    <input placeholder='enter latitude' className='pl-1 w-[80%] border rounded border-black' type="number" name='latitude' />
+                </div>
+                <div className='flex flex-col justify-between'>
+                    <label htmlFor="longitude">longitude:</label>
+                    <input placeholder='enter longitude' className='pl-1 w-[80%] border rounded border-black' type="number" name='longitude' />
+                </div>
+                <div className='flex flex-col justify-between'>
+                    <label htmlFor="city">city:</label>
+                    <input placeholder='enter city' className='pl-1 w-[80%] border rounded border-black' type="text" name='city' />
+                </div>
+            </form>
+        </div>
     )
 }
