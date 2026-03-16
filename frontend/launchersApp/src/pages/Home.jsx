d388c0e7a04e93f324ee2318f31292c7eff6685d
elimelech <ee525566@gmail.com>
2026-03-16 12:46:38 +0200

    setup Home page

--- frontend/launchersApp/src/pages/Home.jsx
+++ frontend/launchersApp/src/pages/Home.jsx
@@ -6,13 +6,13 @@ export default function Home({ launchers }) {
     const [typeFilter, setTypeFilter] = useState('')
     return (
         <>
-            <div>
-                <label htmlFor="search">search:</label>
-                <input className='border rounded border-gray-600' type="text" placeholder='search' onChange={e => setSearch(e.target.value)} />
+            <div className='pl-3'>
+                <label htmlFor="search">search: </label>
+                <input className='pl-1 border rounded border-gray-600' type="text" placeholder='search' onChange={e => setSearch(e.target.value)} />
             </div>
-            <SelectRocketType firstOption='All' setFunction={setTypeFilter} />
-            <Launcher launcher={{ id: 'id', city: 'city', rocketType: 'rocketType', latitude: 'latitude', longitude: 'longitude', name: 'name' }} />
-            { launchers.filter(launcher => typeFilter === 'All'? launcher: launcher.rocketType === typeFilter).filter(launcher => launcher.city.includes(search)).map(l => l && <Launcher launcher={l} />)}
+            <SelectRocketType firstOption='All' setFunction={setTypeFilter} flex_direction='row' />
+            <Launcher onClick={()=>{}} launcher={{ id: 'id', city: 'city', rocketType: 'rocketType', latitude: 'latitude', longitude: 'longitude', name: 'name' }} />
+            {launchers.filter(launcher => typeFilter === 'All'? launcher: launcher.rocketType === typeFilter).filter(launcher => launcher.city.includes(search)).map(l => l && <Launcher onClick={()=>{}} launcher={l} />)}
         </>
     )
 }
